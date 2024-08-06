df41411e0b365c9726d78b1e06a3b4d05f77384a
blackhat <1538658+itsthatblackhat@users.noreply.github.com>
2024-08-05 20:39:47 -0500

    Delete src directory

--- src/index.js
+++
@@ -1,6 +0,0 @@
-const midiHandler = require('./midiHandler');
-const keyMapper = require('./keyMapper');
-
-midiHandler.initialize(keyMapper.mapMidiToKey);
-
-console.log("Listening for MIDI inputs...");
